727b451842e38f2d1295930bb82c88daa20633b3
Ivan Duarte <ivan.nehuen.duarte@gmail.com>
2024-05-18 21:29:03 -0300

    Proyectos, media querie y tecnologías

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
 import Hero from "./components/Hero/Hero";
 import { Loader } from "@react-three/drei";
 import About from "./components/About/About";
+import Project from "./components/Projects/Project";
+import ProjectMobile from "./components/Projects/ProjectMobile";
+import Technologies from "./components/Technologies/Technologies";
+import Poster from "./components/Poster";
 
 function App() {
+  const [isDeskTop, setIsDeskTop] = useState(window.innerWidth > 768);
+
+  useEffect(() => {
+    const handleSize = () => {
+      setIsDeskTop(window.innerWidth > 768);
+    };
+    window.addEventListener("resize", handleSize);
+
+    return () => {
+      window.removeEventListener("resize", handleSize);
+    };
+  });
+
   return (
     <>
       <BrowserRouter>
@@ -15,6 +32,15 @@ function App() {
         </Suspense>
         <Loader />
         <About />
+        {isDeskTop ? (
+          <Suspense fallback={null}>
+            <Project />
+          </Suspense>
+        ) : (
+          <ProjectMobile />
+        )}
+        <Technologies />
+        <Poster />
       </BrowserRouter>
     </>
   );
---
+++ src/components/Projects/ProjectMobile.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { useSpring, animated } from "@react-spring/web";
+
+const ProjectMobile = () => {
+  const imageCount = 9;
+  const springConfig = { mass: 5, tension: 350, friction: 40 };
+  const imageProps = Array.from({ length: imageCount }, () => {
+    const [props, api] = useSpring(() => ({
+      xys: [0, 0, 1],
+      config: springConfig,
+    }));
+    return { props, api };
+  });
+
+  const imageLinks = [
+    "https://hotel-booking-app-rose.vercel.app/",
+    "https://next-js-portfolio-eight-lovat.vercel.app",
+    "https://react-blog-app-delta.vercel.app",
+    "https://hero-landing-page-mu.vercel.app",
+    "https://ed-tech-responsive-website.vercel.app",
+    "https://cafe-website-lemon.vercel.app",
+    "/",
+    "https://simple-portfolio-website-sigma.vercel.app",
+    "/",
+  ];
+
+  return (
+    <div id="projects" className="w-full mx-auto mt-32 mb-40">
+      <div className="text-center mb-20">
+        <span className="border-b-2 text-4xl pink-text-gradient border-pink-600">
+          Projects
+        </span>
+        <div className="mt-5 text-xl text-neutral-200">
+          My Works and Projects
+        </div>
+      </div>
+      <div className="w-full max-auto px-2">
+        <div className="grid gap-4 xl:grid-cols-3 md:grid-cols-2 grid-cols-1">
+          {imageProps.map((image, index) => (
+            <div className="w-full h-[250px] relative" key={index}>
+              <a
+                href={imageLinks[index]}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  className="object-cover w-full h-full absolute"
+                  src={`/project${index + 1}.png`}
+                  alt=""
+                />
+              </a>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectMobile;
